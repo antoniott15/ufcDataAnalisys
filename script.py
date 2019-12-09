8c9bef08f8c02f3f03812dff7449faf8e71f2da0
antoniott15 <antonio.toche@utec.edu.pe>
2019-12-08 23:29:29 -0500

    first step parsing

--- script.py
+++ script.py
@@ -1,12 +1,23 @@
+import csv
+import pandas as pd
+import numpy as np
 
 
 
-
+def init(row):
+    df = pd.DataFrame(row)
+    des = df.describe()
+    des.replace(np.nan,"0")
+    print(des)
 
 
 def main():
-    f = open("./ufcdata/data.csv", "r")
-    print(f.read())
+    with open('./ufcdata/data.csv', newline='') as File:  
+        reader = csv.reader(File)
+        rows = []
+        for row in reader:
+            rows.append(row)
+        init(rows)
 
 
 main()
