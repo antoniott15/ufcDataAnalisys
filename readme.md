f19b29780becf3092e02eee4bb4d476123d5e713
antoniott15 <antonio.toche@utec.edu.pe>
2020-03-09 23:05:09 -0500

    add avrage

--- readme.md
+++ readme.md
@@ -2,6 +2,14 @@
 
 This project is a proof of concept my approach with data science. 
 
+
+# Analysis
+
+For every fighter between the proposed years we have a median of the years.
+
+![Age media]("./avrAge.png")
+
+
 # Data 
 
 I get the data here https://www.kaggle.com/rajeevw/ufcdata/data
--- script.py
+++ script.py
@@ -1,23 +1,36 @@
 import csv
 import pandas as pd
 import numpy as np
+import collections
+import matplotlib.pyplot as plt
+
+# Counter of Red corder
+#      ages
+#
+def averageAge(df):
+    c = collections.Counter(df['R_age'])
+    height = []
+    repetitions = []
+    for values in c.items():
+        height.append(values[0])
+        repetitions.append(values[1])
+
+    fig, axes =  plt.subplots()
+    axes.bar(height,repetitions)
+    fig.suptitle('Average age of fighters')
+    return
 
 
 
-def init(row):
-    df = pd.DataFrame(row)
-    des = df.describe()
-    des.replace(np.nan,"0")
-    print(des)
+def init(df):
+    averageAge(df)
+    plt.show()
+
 
 
 def main():
-    with open('./ufcdata/data.csv', newline='') as File:  
-        reader = csv.reader(File)
-        rows = []
-        for row in reader:
-            rows.append(row)
-        init(rows)
+    df = pd.read_csv('./ufcdata/data.csv')
+    init(df)
 
 
 main()
